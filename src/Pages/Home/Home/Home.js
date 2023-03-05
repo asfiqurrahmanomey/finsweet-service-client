2d1c1378086ef3f4b80feb8025fa5e251debd6c9
asfiqurrahmanomey <omeyjkt@gmail.com>
2023-03-06 02:40:35 +0600

    header_push

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../Hero/Hero';
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <Hero></Hero>
         </div>
     );
 };
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import About from "../../Pages/About/About";
 import Home from "../../Pages/Home/Home/Home";
 
 // Router configuration
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/about',
+                element: <About></About>
             }
         ]
 
